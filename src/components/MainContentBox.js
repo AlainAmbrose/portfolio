74fca906532661bed610d6412fc6686ad0f66139
AlainAmbrose <alainambrose@knights.ucf.edu>
2023-11-01 20:21:33 -0400

    added basic structure for homepage

---
+++ src/components/MainContentBox.js
@@ -0,0 +1,14 @@
+import "../App.css";
+
+function Content() {
+  return (
+    <div>
+      <header>
+        <h1> Alain Ambrose </h1>
+        <h2>Developer</h2>
+      </header>
+    </div>
+  );
+}
+
+export default Content;
