7732b5b66a1a86d89474e72d4d2521d627e4f973
AlainAmbrose <alainambrose@knights.ucf.edu>
2023-12-06 23:37:02 -0500

    added navigation list and main div

--- src/components/MainContentBox.js
+++ src/components/MainContentBox.js
@@ -1,12 +1,50 @@
 import "../App.css";
 
 function Content() {
+  const boxStyle = {
+    margin: "2.5vh",
+    padding: "20px",
+    border: "1px solid #000",
+    height: "95vh",
+    boxSizing: "border-box",
+    borderRadius: "10px",
+    backgroundColor: "#f0f0f0",
+    opacity: 0.5,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  };
+
+  const linksContainerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-end", // Align items to the end (right)
+  };
+
   return (
-    <div>
-      <header>
+    <div style={boxStyle}>
+      <div>
+        {/* <header> */}
+        <h2> Hello, I'm </h2>
         <h1> Alain Ambrose </h1>
-        <h2>Developer</h2>
-      </header>
+        {/* </header> */}
+      </div>
+      <div style={linksContainerStyle}>
+        <ul class="navigation">
+          <li class="navLi">
+            <a>About</a>
+          </li>
+          <li class="navLi">
+            <a>Skills</a>
+          </li>
+          <li class="navLi">
+            <a>Projects</a>
+          </li>
+          <li class="navLi">
+            <a>Experience</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
