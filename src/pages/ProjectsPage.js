f261aa32603da15daeaa63a233c4dd7968e7b084
AlainAmbrose <alainambrose@knights.ucf.edu>
2024-01-12 16:42:48 -0500

    Added chevron icon and made navigation a little better

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -3,12 +3,16 @@ import GitHubSVG from "../images/GitHub.svg";
 import LinkedInSVG from "../images/LinkedIn.svg";
 import InstagramSVG from "../images/Instagram.svg";
 import ProjectCard from "../components/ProjectCard";
+import { ChevronLeftIcon } from "@heroicons/react/20/solid";
+import { useNavigate } from "react-router-dom";
 
 function ProjectsPage() {
+  const navigate = useNavigate();
+
   const topDivStyle = {
     display: "flex",
     backgroundColor: "#f0f0f0",
-    flexDirection: "column",
+    flexDirection: "row",
     justifyContent: "space-between",
   };
 
@@ -27,6 +31,15 @@ function ProjectsPage() {
         <div style={{ flex: "0 0 15%" }}>
           <h1 style={{ fontSize: "5vw", fontWeight: "800" }}> PROJECTS </h1>
         </div>
+        <button
+          style={{ height: "75px", width: "75px" }}
+          onClick={() => navigate("/")}
+        >
+          <ChevronLeftIcon
+            style={{ height: "75px", width: "75px" }}
+            aria-hidden="true"
+          />
+        </button>
       </div>
       <div style={bottomDivStyle}>
         <ProjectCard project={"placeholder"} />
