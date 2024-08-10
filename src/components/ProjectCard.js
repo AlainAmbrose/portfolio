cbbbe3b3fa00d9f019840e3be067bfe5e25e8168
AlainAmbrose <alainambrose@knights.ucf.edu>
2024-08-10 18:04:55 -0400

    added project popup and screen overlay

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,32 @@
 import "../App.css";
 import "../App.css";
+import ProjectPopup from "./ProjectPopup";
+import { useState } from "react";
 
 // projects will be objects that have a title, picture,
 // and description
 const ProjectCard = ({ title }) => {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="project-card">
-      <div style={{ flex: "0 0 70%" }}>
-        <h1 className="project-card-title">{title}</h1>
+    <>
+      <div
+        className="project-card"
+        onClick={() => {
+          setOpen(true);
+          console.log("clicked");
+        }}
+      >
+        <div style={{ flex: "0 0 70%" }}>
+          <h1 className="project-card-title">{title}</h1>
+        </div>
       </div>
-    </div>
+
+      <ProjectPopup
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        description="This is a project description"
+      />
+    </>
   );
 };
 
