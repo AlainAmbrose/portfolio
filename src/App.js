b2c0f192a4dafbeef71b1be882a0f809e9c02859
AlainAmbrose <alainambrose@knights.ucf.edu>
2024-08-17 10:04:55 -0400

    removed skills page from app.js again

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import LandingPage from "./pages/LandingPage.js";
 import AboutPage from "./pages/AboutPage.js";
-import SkillsPage from "./pages/SkillsPage.js";
 import ProjectsPage from "./pages/ProjectsPage.js";
 import ExperiencePage from "./pages/ExperiencePage.js";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
